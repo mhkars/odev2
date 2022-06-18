75fa56aee123fe4ef46e4a64eb3cc46f8bbd81e4
mhkars <mhkars@ASUS-N551JW>
2022-06-18 17:27:56 +0300

    22.06.17_odev

---
+++ src/mustafahoca/com/odev1.java
@@ -0,0 +1,31 @@
+package mustafahoca.com;
+
+import java.util.Scanner;
+
+public class odev1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("choose an dimension");
+		int dimension = scanner.nextInt();
+		System.out.println("choose an odd number");
+		int max = scanner.nextInt();
+
+		int average = (max + 1) / 2;
+		int a = 1;
+		for (int i = 0; i < dimension; i++) {
+			System.out.print("\n");
+			for (int j = 0; j < dimension; j++) {
+				if (i == j)
+					System.out.print(average + " ");
+				else if (i < j)
+					System.out.print(max + " ");
+				else {
+					System.out.print(a + " ");
+				}
+
+			}
+		}
+	}
+
+}
---
+++ src/mustafahoca/com/odev2.java
@@ -0,0 +1,30 @@
+package mustafahoca.com;
+
+import java.util.Scanner;
+
+public class odev2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter a text");
+		String text = scanner.nextLine();
+		int total = 0;
+		char temp;
+
+		for (int i = 0; i < text.length(); i++) {
+			temp = text.charAt(i);
+			for (int j = 0; j < text.length(); j++) {
+				if (temp == text.charAt(j) && temp != ' ') {
+					total += 1;
+				}
+			}
+
+			if (total > 1) {
+				System.out.println("character" + text.charAt(i) + " accours " + total + "times");
+			}
+			text = text.replace(text.charAt(i), ' ');
+			total = 0;
+		}
+
+	}
+}
